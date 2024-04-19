e731373f8c36897a96166e6db3a26baf96ec3ef3
Evgeny Ivanov <es.ivanov@zenit.ru>
2024-04-19 20:38:39 +0500

    Добавил тестирование. Тест handler'a (единственного)

--- Makefile
+++ Makefile
@@ -2,4 +2,8 @@
 build:
 	go build -o ./apiserver ./cmd/apiserver
 
+.PHONY: test
+test:
+	go test -v -race -timeout 30s ./...
+
 .DEFAULT_GOAL := build
---
+++ internal/app/apiserver/apiserver_internal_test.go
@@ -0,0 +1,18 @@
+package apiserver
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestApiServer_HandleHello(t *testing.T) {
+	s := New(NewConfig())
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/hello", nil)
+	s.handleHello().ServeHTTP(rec, req)
+
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.Equal(t, "Hello World", rec.Body.String())
+}
